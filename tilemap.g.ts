ba8889aeb44a7d3703573a3d955e51e5feca19aa
Gustav X <sillygorilla42@gmail.com>
2025-05-04 14:24:32 +0200

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -4,25 +4,31 @@ namespace myTiles {
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`12000d00010101010101010101010000000000010101010000000100000000010001000100010101000001000000010100010001000100000001000101010000000000000000000101000101000000000001010000010001000000000000010101010000000000010001010000000001010100000100000100010000000001000001010000000001000100010001000001000001010001000000000000000001000000000000010001000101000100010001000001000000010000000000000100010001000001010101000000010000010101010000000101010101010101010101010101010101010101010101`, img`
+            case "level1":return tiles.createTilemap(hex`12000d00010101010101010101010000000000010101010000000100000000010000000000000101000000000000000000010000000100000001000000000000000000000000000100000001000002000000010000010000000000000000010101010000000000000000000000000000010000000000000000000000000001000001000000000000000000000000000000000001000001000000000000000001000000000000000001000001010100000000000001000000000000000000010104000000000001010101030000000000010101010000000101010101010101010101010101010101010101010101`, img`
 2 2 2 2 2 2 2 2 2 2 . . . . . 2 2 2 
-2 . . . 2 . . . . 2 . 2 . 2 . 2 2 2 
-. . 2 . . . 2 2 . 2 . 2 . 2 . . . 2 
-. 2 2 2 . . . . . . . . . 2 2 . 2 2 
-. . . . . 2 2 . . 2 . 2 . . . . . . 
-2 2 2 2 . . . . . 2 . 2 2 . . . . 2 
-2 2 . . 2 . . 2 . 2 . . . . 2 . . 2 
-2 . . . . 2 . 2 . 2 . 2 . . 2 . . 2 
-2 . 2 . . . . . . . . 2 . . . . . . 
-2 . 2 . 2 2 . 2 . 2 . 2 . . 2 . . . 
-2 . . . . . . 2 . 2 . 2 . . 2 2 2 2 
-. . . 2 . . 2 2 2 2 . . . 2 2 2 2 2 
+2 . . . 2 . . . . 2 . . . . . . 2 2 
+. . . . . . . . . 2 . . . 2 . . . 2 
+. . . . . . . . . . . . . 2 . . . 2 
+. . . . . . 2 . . 2 . . . . . . . . 
+2 2 2 2 . . . . . . . . . . . . . . 
+2 . . . . . . . . . . . . . 2 . . 2 
+. . . . . . . . . . . . . . . . . 2 
+. . 2 . . . . . . . . 2 . . . . . . 
+. . 2 . . 2 2 2 . . . . . . 2 . . . 
+. . . . . . 2 2 . . . . . . 2 2 2 2 
+. . . . . . 2 2 2 2 . . . 2 2 2 2 2 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile1], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4], TileScale.Sixteen);
         }
         return null;
     })
@@ -32,6 +38,12 @@ namespace myTiles {
             case "transparency16":return transparency16;
             case "myTile":
             case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
         }
         return null;
     })
